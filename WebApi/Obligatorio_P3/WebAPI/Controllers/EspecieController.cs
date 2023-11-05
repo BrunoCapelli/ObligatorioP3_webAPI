406dea4f18a9ad9e04acaede5aecdfa86414bd05
Bruno Capelli <brunoicapelli@gmail.com>
2023-11-05 13:22:44 -0300

    First GET by API :´)

---
+++ WebApi/Obligatorio_P3/WebAPI/Controllers/EspecieController.cs
@@ -0,0 +1,50 @@
+﻿using Domain.DTO;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.IServicios;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EspecieController : ControllerBase
+    {
+
+        private IServicioEspecie _servicioEspecie;
+
+        public EspecieController(IServicioEspecie servicioEspecie)
+        {
+            _servicioEspecie = servicioEspecie;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<EspecieDTO> especies = _servicioEspecie.GetAll();
+            return Ok(especies);
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebApi/Obligatorio_P3/WebAPI/Program.cs
+++ WebApi/Obligatorio_P3/WebAPI/Program.cs
@@ -1,4 +1,11 @@
 
+using Data_Access;
+using Data_Access.IRepositorios;
+using Data_Access.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using Servicios.IServicios;
+using Servicios.Servicios;
+
 namespace WebAPI
 {
     public class Program
@@ -13,6 +20,36 @@ namespace WebAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DbContext, MiContexto>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Connection"));
+            });
+            builder.Services.AddScoped(typeof(IServicioAudit), typeof(ServicioAudit));
+            builder.Services.AddScoped(typeof(IServicioAmenaza), typeof(ServicioAmenaza));
+            builder.Services.AddScoped(typeof(IServicioEspecie), typeof(ServicioEspecie));
+            builder.Services.AddScoped(typeof(IServicioEstadoConservacion), typeof(ServicioEstadoConservacion));
+            builder.Services.AddScoped(typeof(IServicioPais), typeof(ServicioPais));
+            builder.Services.AddScoped(typeof(IServicioEcosistemaMarino), typeof(ServicioEcosistemaMarino));
+            builder.Services.AddScoped(typeof(IServicioEcosistemaMarinoEspecie), typeof(ServicioEcosistemaMarinoEspecie));
+            builder.Services.AddScoped(typeof(IServicioEcosistemaAmenaza), typeof(ServicioEcosistemaAmenaza));
+            builder.Services.AddScoped(typeof(IServicioUsuario), typeof(ServicioUsuario));
+            builder.Services.AddScoped(typeof(IServicioEspecieAmenaza), typeof(ServicioEspecieAmenaza));
+
+
+            builder.Services.AddScoped(typeof(IRepositorioAudit), typeof(RepositorioAudit));
+            builder.Services.AddScoped(typeof(IRepositorioAmenaza), typeof(RepositorioAmenaza));
+            builder.Services.AddScoped(typeof(IRepositorioEspecieAmenaza), typeof(RepositorioEspecieAmenaza));
+            builder.Services.AddScoped(typeof(IRepositorioEspecie), typeof(RepositorioEspecie));
+            builder.Services.AddScoped(typeof(IRepositorioEstadoConservacion), typeof(RepositorioEstadoConservacion));
+            builder.Services.AddScoped(typeof(IRepositorioPais), typeof(RepositorioPais));
+            builder.Services.AddScoped(typeof(IRepositorioEcosistemaMarino), typeof(RepositorioEcosistemaMarino));
+            builder.Services.AddScoped(typeof(IRepositorioEcosistemaAmenaza), typeof(RepositorioEcosistemaAmenaza));
+            builder.Services.AddScoped(typeof(IRepositorioEcosistemaMarinoEspecie), typeof(RepositorioEcosistemaMarinoEspecie));
+            builder.Services.AddScoped(typeof(IRepositorioUsuario), typeof(RepositorioUsuario));
+
+
+
 
             var app = builder.Build();
 
