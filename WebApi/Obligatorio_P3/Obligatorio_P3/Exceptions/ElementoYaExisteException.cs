ea06188f90c7a7b85f7f8f076c25b5b2e641c225
Bruno Capelli <brunoicapelli@gmail.com>
2023-11-05 14:57:47 -0300

    Create GET methods on each HttpGet from ServicioEspecie

---
+++ WebApi/Obligatorio_P3/Obligatorio_P3/Exceptions/ElementoYaExisteException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class ElementoYaExisteException : Exception
+    {
+        public ElementoYaExisteException() { }
+        public ElementoYaExisteException(string message) : base(message) { }
+        public ElementoYaExisteException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
--- WebApi/Obligatorio_P3/WebAPI/Controllers/EspecieController.cs
+++ WebApi/Obligatorio_P3/WebAPI/Controllers/EspecieController.cs
@@ -1,7 +1,9 @@
 ﻿using Domain.DTO;
 using Domain.Entities;
+using Domain.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Servicios.IServicios;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,31 +22,134 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        [ProducesResponseType(StatusCodes.Status200OK)] // Estos chirimbolos son para que Swagger interprete los posibles codigos de error que devuelve y los documente automaticamente
+        public IActionResult GetAll()
         {
             IEnumerable<EspecieDTO> especies = _servicioEspecie.GetAll();
             return Ok(especies);
         }
 
         [HttpGet("{id}")]
-        public string Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                EspecieDTO especie = _servicioEspecie.GetById(id);
+                return Ok(especie);
+
+            }catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
 
+        
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public IActionResult Post([FromBody] EspecieDTO especieDTO)  // WIP: tengo que ver que hago con el tema de la imagen al crear un nuevo objeto
         {
+            try
+            {
+                EspecieDTO eAdd = _servicioEspecie.Add(especieDTO);
+                return Ok(eAdd);
+            }
+            catch(ElementoYaExisteException ex)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+            }
+            catch(ElementoNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
         
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+       
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
         {
+            try
+            {
+                _servicioEspecie.Remove(id);
+                return Ok("Eliminado con exito");
+            }
+            catch (ElementoNoValidoException exception)
+            {
+                return NotFound(exception.Message);
+            }
         }
 
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+
+        [HttpGet("nombre/{nombreCientifico}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetPorNombreCientifico(string nombreCientifico)
         {
+            try
+            {
+                IEnumerable<EspecieDTO> especies = _servicioEspecie.FiltrarPorNombreCientifico(nombreCientifico);
+                return Ok(especies);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
+
+        [HttpGet("grado/{gradoConservacion}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetGradoDeConservacion(int gradoConservacion)
+        {
+            try
+            {
+                IEnumerable<EspecieDTO> especies = _servicioEspecie.FiltrarPorGradoDeConservacion(gradoConservacion);
+                return Ok(especies);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("peso/{pesoDesde}/{pesoHasta}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetPeso(int pesoDesde, int pesoHasta)
+        {
+            try
+            {
+                IEnumerable<EspecieDTO> especies = _servicioEspecie.FiltrarPorPeso(pesoDesde, pesoHasta);
+                return Ok(especies);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("ecosistema/{ecosistemaID}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetPorEcosistema(int ecosistemaID)
+        {
+            try
+            {
+                IEnumerable<EspecieDTO> especies = _servicioEspecie.FiltrarPorEcosistema(ecosistemaID);
+                return Ok(especies);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+
+
     }
 }
