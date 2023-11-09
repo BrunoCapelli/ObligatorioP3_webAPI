d7531fbea3d367418edad4d4a2da01c38c56b5d0
Nicolas Herrera Aguete <NH319004@fi365.ort.edu.uy>
2023-11-08 21:23:44 -0300

    Metodos para API de Amenaza

--- WebApi/Obligatorio_P3/WebAPI/Controllers/AmenazaController.cs
+++ WebApi/Obligatorio_P3/WebAPI/Controllers/AmenazaController.cs
@@ -5,23 +5,42 @@ using Microsoft.AspNetCore.Mvc;
 using Servicios.IServicios;
 using Microsoft.AspNetCore.Authorization;
 using System.ComponentModel.DataAnnotations;
+using Servicios.Servicios;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebAPI.Controllers {
+
     [Route("api/[controller]")]
     [ApiController]
     public class AmenazaController : ControllerBase {
-        // GET: api/<AmenazaController>
-        [HttpGet]
-        public IEnumerable<string> Get() {
-            return new string[] { "value1", "value2" };
+        private IServicioAmenaza _servicioAmenaza;
+
+        public AmenazaController(IServicioAmenaza servicioAmenaza) {
+            _servicioAmenaza = servicioAmenaza;
         }
 
-        // GET api/<AmenazaController>/5
-        [HttpGet("{id}")]
-        public string Get(int id) {
-            return "value";
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)] // Estos chirimbolos son para que Swagger interprete los posibles codigos de error que devuelve y los documente automaticamente
+        public IActionResult GetAll() {
+            IEnumerable<AmenazaDTO> amenazas = _servicioAmenaza.GetAll();
+            return Ok(amenazas);
+        }
+
+        [HttpGet("Ecosistema/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id) {
+
+            try {
+                AmenazaDTO amenaza = _servicioAmenaza.GetAmenaza(id);
+                return Ok(amenaza);
+
+            }
+            catch (Exception ex) {
+                return NotFound(ex.Message);
+            }
         }
 
        
