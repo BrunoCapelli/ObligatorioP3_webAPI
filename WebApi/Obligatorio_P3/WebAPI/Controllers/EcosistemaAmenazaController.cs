f5ac6a0d3cd17da2027bc616c0c950263a996d8e
Nicolas Herrera Aguete <NH319004@fi365.ort.edu.uy>
2023-11-14 21:32:28 -0300

    Subo controladores de clases intermedias

---
+++ WebApi/Obligatorio_P3/WebAPI/Controllers/EcosistemaAmenazaController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.DTO;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.IServicios;
+
+namespace WebAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EcosistemaAmenazaController : ControllerBase {
+        private IServicioEcosistemaAmenaza _servicioEcosistemaAmenaza;
+
+        public EcosistemaAmenazaController(IServicioEcosistemaAmenaza servicioEcostemaAmenaza) {
+            _servicioEcosistemaAmenaza = servicioEcostemaAmenaza;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public IActionResult Post([FromBody] int amenazaId, int ecosistemaId)  
+        { 
+            try {
+                _servicioEcosistemaAmenaza.Add(amenazaId, ecosistemaId);
+                return Ok();
+            }
+            catch (ElementoNoValidoException ex) {
+                return BadRequest(ex.ToString());
+            
+            }
+        
+        }
+    }
+}
---
+++ WebApi/Obligatorio_P3/WebAPI/Controllers/EspecieAmenazaController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.DTO;
+using Domain.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.IServicios;
+using Servicios.Servicios;
+
+namespace WebAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EspecieAmenazaController : ControllerBase {
+        private IServicioEspecieAmenaza _servicioEspecieAmenaza;
+
+        public EspecieAmenazaController(IServicioEspecieAmenaza servicioEspecieAmenaza) {
+            _servicioEspecieAmenaza = servicioEspecieAmenaza;
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public IActionResult Post([FromBody] int amenazaId, int especieId) {
+            try {
+                _servicioEspecieAmenaza.Add(amenazaId, especieId);
+                return Ok();
+            }
+            catch (ElementoNoValidoException ex) {
+                return BadRequest(ex.ToString());
+            }
+
+        }
+    }
+}
